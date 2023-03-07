1bdea0a7b8115b0808d60d49323d5247e6e38af7
Luke Sandoval <118379340+lukesan48@users.noreply.github.com>
2023-03-07 10:10:19 -0500

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def encode(user_input):
-    encoded_list = [int(user_input[i]) + 3 for i in range(len(user_input))]
-    encoded_pass = [str(element) for element in encoded_list]
-    encoded_pass = ''. join(encoded_pass)
+def encode(user_input):  # Luke Sandoval
+    encoded_list = [int(user_input[i]) + 3 for i in range(len(user_input))]  # create list with each element + 3
+    encoded_pass = [str(element) for element in encoded_list]  # conv each element into str
+    encoded_pass = ''. join(encoded_pass)  # join elements into str
     return encoded_pass
 
 def decode(user_input):
@@ -10,19 +10,19 @@ def decode(user_input):
 def menu():
     print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
 
-option = 0
-while option != 3:
-    menu()
-    option = int(input("Please enter an option: "))
-    if option == 1:
-        user_input = str(input("Please enter your password to encode: "))
-        encoded_pass = encode(user_input)
-        print("Your password has been encoded and stored!")
-        print()
-    elif option == 2:
-        pass
-        #decoded_pass = decode(encoded_pass)
-        #print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
-        #print()
-    elif option == 3:
-        break
+if __name__ == "__main__":
+    option = 0
+    while option != 3:
+        menu()
+        option = int(input("Please enter an option: "))  # prompt user for option
+        if option == 1:
+            user_input = str(input("Please enter your password to encode: "))
+            encoded_pass = encode(user_input)  # encode user_input
+            print("Your password has been encoded and stored!")
+            print()
+        elif option == 2:
+            decoded_pass = decode(encoded_pass)  # decode the encoded user_input
+            print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
+            print()
+        elif option == 3:
+            break
