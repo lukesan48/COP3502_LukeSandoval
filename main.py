a6a640276e2b356b654ecb24446731885146ec0b
Luke Sandoval <118379340+lukesan48@users.noreply.github.com>
2023-03-07 09:47:26 -0500

    created main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+def encode(user_input):
+    encoded_list = [int(user_input[i]) + 3 for i in range(len(user_input))]
+    encoded_pass = [str(element) for element in encoded_list]
+    encoded_pass = ''. join(encoded_pass)
+    return encoded_pass
+
+
+def menu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+
+option = 0
+while option != 3:
+    menu()
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        user_input = str(input("Please enter your password to encode: "))
+        encoded_pass = encode(user_input)
+        print("Your password has been encoded and stored!")
+        print()
+    elif option == 2:
+        decoded_pass = decode(encoded_pass)
+        print(f"The encoded password is {encoded_pass}, and the original password is {decoded_pass}.")
+        print()
+    elif option == 3:
+        break
